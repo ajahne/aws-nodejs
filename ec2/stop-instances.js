ad1d498f2e3f7bdf1a42b7062444e3b2b17d63df
Ajahne Santa Anna <ajahne@gmail.com>
2019-04-19 23:13:43 -0400

    Update stop-instances.js

--- ec2/stop-instances.js
+++ ec2/stop-instances.js
@@ -7,14 +7,14 @@ AWS.config.update({region:'us-west-2'});
 const ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
 //setup instance params
-const instanceParams = {
+const params = {
   DryRun: true,
   InstanceIds: [
     'i-#####'    
   ]
 };
 
-ec2.stopInstances(instanceParams, function(err, data) {
+ec2.stopInstances(params, function(err, data) {
   if (err) {
     console.log(err, err.stack); // an error occurred
   } else {
